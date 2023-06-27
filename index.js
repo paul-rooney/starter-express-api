9c165b8fe2d8fa920a7c4d630e619e51706e9e65
Paul Rooney <paulrooney90@gmail.com>
2023-06-27 11:47:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const authToken = process.env.authToken;
 
 app.all('/', (req, res) => {
     console.log("Just got a request!", authToken)
-    res.send('Yo!')
+    res.send('Yo!', authToken)
 })
 app.listen(process.env.PORT || 3000)
