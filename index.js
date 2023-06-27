ecf120e310d80f80cd10a456bb9610c3cdf30141
Paul Rooney <paulrooney90@gmail.com>
2023-06-27 11:49:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 
-const authToken = process.env.authToken;
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 
 app.all('/', (req, res) => {
-    console.log("Just got a request!", authToken)
-    res.send('Yo!', authToken)
+    console.log("Just got a request!", authToken, accountSid)
+    res.send('Yo!', authToken, accountSid)
 })
 app.listen(process.env.PORT || 3000)
