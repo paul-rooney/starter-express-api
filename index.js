42199c65b871f544948fb574d70018b986ea8571
Paul Rooney <75624065+paul-rooney@users.noreply.github.com>
2023-06-27 16:26:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 const cron = require("node-cron");
 const twilio = require("twilio");
 
-// const accountSid = process.env.TWILIO_ACCOUNT_SID;
-// const authToken = process.env.TWILIO_AUTH_TOKEN;
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = twilio(accountSid, authToken);
 
 app.use(express.json());
